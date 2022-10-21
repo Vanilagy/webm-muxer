420a5fa2bc68b62757bed88d74f16665909df6ca
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2022-10-21 20:45:40 +0200

    Implement main muxer code

--- build/bundle.js
+++ build/bundle.js
@@ -1,13 +1,37 @@
 "use strict";
 (() => {
   // src/main.ts
+  var VIDEO_TRACK_NUMBER = 1;
+  var AUDIO_TRACK_NUMBER = 2;
+  var MAX_CHUNK_LENGTH_MS = 32e3;
+  var EBMLFloat32 = class {
+    value;
+    constructor(value) {
+      this.value = value;
+    }
+  };
+  var EBMLFloat64 = class {
+    value;
+    constructor(value) {
+      this.value = value;
+    }
+  };
   var WriteTarget = class {
     pos = 0;
     helper = new Uint8Array(8);
     helperView = new DataView(this.helper.buffer);
+    offsets = /* @__PURE__ */ new WeakMap();
     writeU8(value) {
       this.helperView.setUint8(0, value);
       this.write(this.helper.subarray(0, 1));
+    }
+    writeFloat32(value) {
+      this.helperView.setFloat32(0, value, false);
+      this.write(this.helper.subarray(0, 4));
+    }
+    writeFloat64(value) {
+      this.helperView.setFloat64(0, value, false);
+      this.write(this.helper);
     }
     writeUnsignedInt(value, width = measureUnsignedInt(value)) {
       switch (width) {
@@ -63,11 +87,14 @@
       this.write(new Uint8Array(str.split("").map((x) => x.charCodeAt(0))));
     }
     writeEBML(data) {
-      if (Array.isArray(data)) {
+      if (data instanceof Uint8Array) {
+        this.write(data);
+      } else if (Array.isArray(data)) {
         for (let elem of data) {
           this.writeEBML(elem);
         }
       } else {
+        this.offsets.set(data, this.pos);
         this.writeUnsignedInt(data.id);
         if (typeof data.data === "number") {
           let size = data.size ?? measureUnsignedInt(data.data);
@@ -86,6 +113,15 @@
         } else if (typeof data.data === "string") {
           this.writeEBMLVarInt(data.data.length);
           this.writeString(data.data);
+        } else if (data.data instanceof Uint8Array) {
+          this.writeEBMLVarInt(data.data.byteLength);
+          this.write(data.data);
+        } else if (data.data instanceof EBMLFloat32) {
+          this.writeEBMLVarInt(4);
+          this.writeFloat32(data.data.value);
+        } else if (data.data instanceof EBMLFloat64) {
+          this.writeEBMLVarInt(8);
+          this.writeFloat64(data.data.value);
         }
       }
     }
@@ -147,41 +183,264 @@
       document.body.removeChild(a);
     }, 0);
   };
-  var target = new ArrayBufferWriteTarget();
-  target.writeEBML({
-    "id": 440786851,
-    "data": [
-      {
-        "id": 17030,
-        "data": 1
-      },
-      {
-        "id": 17143,
-        "data": 1
-      },
-      {
-        "id": 17138,
-        "data": 4
-      },
-      {
-        "id": 17139,
-        "data": 8
-      },
-      {
-        "id": 17026,
-        "data": "webm"
-      },
-      {
-        "id": 17031,
-        "data": 2
-      },
-      {
-        "id": 17029,
-        "data": 2
+  var WebMWriter = class {
+    target;
+    options;
+    segment;
+    segmentInfo;
+    tracksElement;
+    currentCluster;
+    currentClusterTimestamp;
+    segmentDuration;
+    audioCodecPrivate;
+    cues;
+    seekHead;
+    duration = 0;
+    videoChunkQueue = [];
+    audioChunkQueue = [];
+    lastVideoTimestamp = 0;
+    lastAudioTimestamp = 0;
+    constructor(options) {
+      this.target = new ArrayBufferWriteTarget();
+      this.options = options;
+      this.writeHeader();
+    }
+    writeHeader() {
+      let ebmlHeader = { id: 440786851, data: [
+        { id: 17030, data: 1 },
+        { id: 17143, data: 1 },
+        { id: 17138, data: 4 },
+        { id: 17139, data: 8 },
+        { id: 17026, data: "webm" },
+        { id: 17031, data: 2 },
+        { id: 17029, data: 2 }
+      ] };
+      this.target.writeEBML(ebmlHeader);
+      let seekHead = { id: 290298740, data: [
+        { id: 19899, data: [
+          { id: 21419, data: new Uint8Array([28, 83, 187, 107]) },
+          { id: 21420, size: 5, data: 0 }
+        ] },
+        { id: 19899, data: [
+          { id: 21419, data: new Uint8Array([21, 73, 169, 102]) },
+          { id: 21420, size: 5, data: 0 }
+        ] },
+        { id: 19899, data: [
+          { id: 21419, data: new Uint8Array([22, 84, 174, 107]) },
+          { id: 21420, size: 5, data: 0 }
+        ] }
+      ] };
+      this.seekHead = seekHead;
+      let segmentDuration = { id: 17545, data: new EBMLFloat64(0) };
+      this.segmentDuration = segmentDuration;
+      let segmentInfo = { id: 357149030, data: [
+        { id: 2807729, data: 1e6 },
+        { id: 19840, data: "Vani's epic muxer" },
+        { id: 22337, data: "Vani's epic muxer" },
+        segmentDuration
+      ] };
+      this.segmentInfo = segmentInfo;
+      let tracksElement = { id: 374648427, data: [] };
+      this.tracksElement = tracksElement;
+      if (this.options.video) {
+        tracksElement.data.push({ id: 174, data: [
+          { id: 215, data: VIDEO_TRACK_NUMBER },
+          { id: 29637, data: VIDEO_TRACK_NUMBER },
+          { id: 131, data: 1 },
+          { id: 134, data: this.options.video.codec },
+          { id: 224, data: [
+            { id: 176, data: this.options.video.width },
+            { id: 186, data: this.options.video.height }
+          ] }
+        ] });
       }
-    ]
-  });
-  var buffer = target.finalize();
-  console.log(buffer);
-  saveFile(new Blob([buffer]));
+      if (this.options.audio) {
+        this.audioCodecPrivate = { id: 25506, data: new Uint8Array(19) };
+        tracksElement.data.push({ id: 174, data: [
+          { id: 215, data: AUDIO_TRACK_NUMBER },
+          { id: 29637, data: AUDIO_TRACK_NUMBER },
+          { id: 131, data: 2 },
+          { id: 134, data: this.options.audio.codec },
+          this.audioCodecPrivate,
+          { id: 225, data: [
+            { id: 181, data: new EBMLFloat32(this.options.audio.sampleRate) },
+            { id: 159, data: this.options.audio.numberOfChannels }
+          ] }
+        ] });
+      }
+      let segment = { id: 408125543, size: 5, data: [
+        seekHead,
+        segmentInfo,
+        tracksElement
+      ] };
+      this.segment = segment;
+      this.target.writeEBML(segment);
+      this.cues = { id: 475249515, data: [] };
+    }
+    addVideoChunk(chunk) {
+      this.lastVideoTimestamp = chunk.timestamp;
+      console.log(chunk);
+      while (this.audioChunkQueue.length > 0 && this.audioChunkQueue[0].timestamp <= chunk.timestamp) {
+        let audioChunk = this.audioChunkQueue.shift();
+        this.writeSimpleBlock(audioChunk);
+      }
+      if (!this.options.audio || chunk.timestamp <= this.lastAudioTimestamp) {
+        this.writeSimpleBlock(chunk);
+        this.lastVideoTimestamp = chunk.timestamp;
+      } else {
+        this.videoChunkQueue.push(chunk);
+      }
+    }
+    addAudioChunk(chunk, meta) {
+      this.lastAudioTimestamp = chunk.timestamp;
+      console.log(chunk);
+      while (this.videoChunkQueue.length > 0 && this.videoChunkQueue[0].timestamp <= chunk.timestamp) {
+        let videoChunk = this.videoChunkQueue.shift();
+        this.writeSimpleBlock(videoChunk);
+      }
+      if (!this.options.video || chunk.timestamp <= this.lastVideoTimestamp) {
+        this.writeSimpleBlock(chunk);
+        this.lastAudioTimestamp = chunk.timestamp;
+      } else {
+        this.audioChunkQueue.push(chunk);
+      }
+      if (meta?.decoderConfig) {
+        this.audioCodecPrivate.data = new Uint8Array(meta.decoderConfig.description);
+        let endPos = this.target.pos;
+        this.target.seek(this.target.offsets.get(this.audioCodecPrivate));
+        this.target.writeEBML(this.audioCodecPrivate);
+        this.target.seek(endPos);
+      }
+    }
+    writeSimpleBlock(chunk) {
+      let msTime = Math.floor(chunk.timestamp / 1e3);
+      if (!this.currentCluster || (chunk instanceof EncodedVideoChunk && chunk.type === "key" || msTime - this.currentClusterTimestamp >= MAX_CHUNK_LENGTH_MS)) {
+        this.createNewCluster(msTime);
+      }
+      let prelude = new Uint8Array(4);
+      let view = new DataView(prelude.buffer);
+      view.setUint8(0, 128 | (chunk instanceof EncodedVideoChunk ? VIDEO_TRACK_NUMBER : AUDIO_TRACK_NUMBER));
+      view.setUint16(1, msTime - this.currentClusterTimestamp, false);
+      view.setUint8(3, Number(chunk.type === "key") << 7);
+      let data = new Uint8Array(chunk.byteLength);
+      chunk.copyTo(data);
+      let simpleBlock = { id: 163, data: [
+        prelude,
+        data
+      ] };
+      this.target.writeEBML(simpleBlock);
+      this.duration = Math.max(this.duration, msTime);
+    }
+    createNewCluster(timestamp) {
+      if (this.currentCluster) {
+        this.finalizeCurrentCluster();
+      }
+      this.currentCluster = { id: 524531317, data: [
+        { id: 231, data: timestamp }
+      ] };
+      this.target.writeEBML(this.currentCluster);
+      this.currentClusterTimestamp = timestamp;
+      this.cues.data.push({ id: 187, data: [
+        { id: 179, data: timestamp },
+        { id: 183, data: [
+          { id: 247, data: VIDEO_TRACK_NUMBER },
+          { id: 241, data: this.target.offsets.get(this.currentCluster) - (this.target.offsets.get(this.segment) + 8) }
+        ] }
+      ] });
+    }
+    finalizeCurrentCluster() {
+      let clusterSize = this.target.pos - (this.target.offsets.get(this.currentCluster) + 8);
+      let endPos = this.target.pos;
+      this.target.seek(this.target.offsets.get(this.currentCluster) + 4);
+      this.target.writeEBMLVarInt(clusterSize, 4);
+      this.target.seek(endPos);
+    }
+    finalize() {
+      while (this.videoChunkQueue.length > 0)
+        this.writeSimpleBlock(this.videoChunkQueue.shift());
+      while (this.audioChunkQueue.length > 0)
+        this.writeSimpleBlock(this.audioChunkQueue.shift());
+      this.finalizeCurrentCluster();
+      this.target.writeEBML(this.cues);
+      let endPos = this.target.pos;
+      let segmentSize = this.target.pos - (this.target.offsets.get(this.segment) + 8);
+      this.target.seek(this.target.offsets.get(this.segment) + 4);
+      this.target.writeEBMLVarInt(segmentSize, 4);
+      this.segmentDuration.data = new EBMLFloat64(this.duration);
+      this.target.seek(this.target.offsets.get(this.segmentDuration));
+      this.target.writeEBML(this.segmentDuration);
+      this.seekHead.data[0].data[1].data = this.target.offsets.get(this.cues) - (this.target.offsets.get(this.segment) + 8);
+      this.seekHead.data[1].data[1].data = this.target.offsets.get(this.segmentInfo) - (this.target.offsets.get(this.segment) + 8);
+      this.seekHead.data[2].data[1].data = this.target.offsets.get(this.tracksElement) - (this.target.offsets.get(this.segment) + 8);
+      this.target.seek(this.target.offsets.get(this.seekHead));
+      this.target.writeEBML(this.seekHead);
+      this.target.seek(endPos);
+    }
+  };
+  (async () => {
+    let sampleRate = 48e3;
+    let writer = new WebMWriter({
+      video: {
+        codec: "V_VP9",
+        width: 1280,
+        height: 720
+      },
+      audio: {
+        codec: "A_OPUS",
+        numberOfChannels: 1,
+        sampleRate
+      }
+    });
+    let canvas = document.createElement("canvas");
+    canvas.setAttribute("width", "1280");
+    canvas.setAttribute("height", "720");
+    let ctx = canvas.getContext("2d");
+    let videoEncoder = new VideoEncoder({
+      output: (chunk) => writer.addVideoChunk(chunk),
+      error: (e) => console.error(e)
+    });
+    videoEncoder.configure({
+      codec: "vp09.00.10.08",
+      width: 1280,
+      height: 720,
+      bitrate: 1e6
+    });
+    let audioEncoder = new AudioEncoder({
+      output: (chunk, meta) => writer.addAudioChunk(chunk, meta),
+      error: (e) => console.error(e)
+    });
+    audioEncoder.configure({
+      codec: "opus",
+      numberOfChannels: 1,
+      sampleRate,
+      bitrate: 32e3
+    });
+    let audioContext = new AudioContext();
+    let audioBuffer = await audioContext.decodeAudioData(await (await fetch("./CantinaBand60.wav")).arrayBuffer());
+    let length = 5;
+    let data = new Float32Array(length * sampleRate);
+    data.set(audioBuffer.getChannelData(0).subarray(0, data.length), 0);
+    let audioData = new AudioData({
+      format: "f32",
+      sampleRate,
+      numberOfFrames: length * sampleRate,
+      numberOfChannels: 1,
+      timestamp: 0,
+      data
+    });
+    audioEncoder.encode(audioData);
+    audioData.close();
+    for (let i = 0; i < length * 5; i++) {
+      ctx.fillStyle = ["red", "lime", "blue", "yellow"][Math.floor(Math.random() * 4)];
+      ctx.fillRect(Math.random() * 1280, Math.random() * 720, Math.random() * 1280, Math.random() * 720);
+      let videoFrame = new VideoFrame(canvas, { timestamp: i * 1e6 / 5 });
+      videoEncoder.encode(videoFrame);
+      videoFrame.close();
+    }
+    await Promise.allSettled([videoEncoder.flush(), audioEncoder.flush()]);
+    writer.finalize();
+    let buffer = writer.target.finalize();
+    console.log(buffer);
+    saveFile(new Blob([buffer]));
+  })();
 })();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
-type EBML = { id: number, data: number | string | EBML[], size?: number } | EBML[];
+const VIDEO_TRACK_NUMBER = 1;
+const AUDIO_TRACK_NUMBER = 2;
+const MAX_CHUNK_LENGTH_MS = 32_000;
+
+interface EBMLElement {
+	id: number,
+	size?: number,
+	data: number | string | Uint8Array | EBMLFloat32 | EBMLFloat64 | EBML[]
+}
+
+type EBML = EBMLElement | Uint8Array | EBML[];
+
+class EBMLFloat32 {
+	value: number;
+
+	constructor(value: number) {
+		this.value = value;
+	}
+}
+
+class EBMLFloat64 {
+	value: number;
+
+	constructor(value: number) {
+		this.value = value;
+	}
+}
 
 abstract class WriteTarget {
 	pos = 0;
 	helper = new Uint8Array(8);
 	helperView = new DataView(this.helper.buffer);
+	offsets = new WeakMap<EBML, number>();
 	
 	abstract write(data: Uint8Array): void;
 	abstract seek(newPos: number): void;
@@ -11,6 +38,16 @@ abstract class WriteTarget {
 	writeU8(value: number) {
 		this.helperView.setUint8(0, value);
 		this.write(this.helper.subarray(0, 1));
+	}
+
+	writeFloat32(value: number) {
+		this.helperView.setFloat32(0, value, false);
+		this.write(this.helper.subarray(0, 4));
+	}
+
+	writeFloat64(value: number) {
+		this.helperView.setFloat64(0, value, false);
+		this.write(this.helper);
 	}
 
 	writeUnsignedInt(value: number, width: number = measureUnsignedInt(value)) {
@@ -75,11 +112,15 @@ abstract class WriteTarget {
 	}
 
 	writeEBML(data: EBML) {
-		if (Array.isArray(data)) {
+		if (data instanceof Uint8Array) {
+			this.write(data);
+		} else if (Array.isArray(data)) {
 			for (let elem of data) {
 				this.writeEBML(elem);
 			}
 		} else {
+			this.offsets.set(data, this.pos);
+
 			this.writeUnsignedInt(data.id); // ID field
 
 			if (typeof data.data === 'number') {
@@ -102,6 +143,15 @@ abstract class WriteTarget {
 			} else if (typeof data.data === 'string') {
 				this.writeEBMLVarInt(data.data.length);
 				this.writeString(data.data);
+			} else if (data.data instanceof Uint8Array) {
+				this.writeEBMLVarInt(data.data.byteLength);
+				this.write(data.data);
+			} else if (data.data instanceof EBMLFloat32) {
+				this.writeEBMLVarInt(4);
+				this.writeFloat32(data.data.value);
+			} else if (data.data instanceof EBMLFloat64) {
+				this.writeEBMLVarInt(8);
+				this.writeFloat64(data.data.value);
 			}
 		}
 	}
@@ -177,42 +227,344 @@ const saveFile = (blob: Blob, filename = 'unnamed.webm') => {
 	}, 0);
 };
 
-let target = new ArrayBufferWriteTarget();
-target.writeEBML({
-	'id': 0x1a45dfa3,  // EBML
-	'data': [
-	  {
-		'id': 0x4286,  // EBMLVersion
-		'data': 1
-	  },
-	  {
-		'id': 0x42f7,  // EBMLReadVersion
-		'data': 1
-	  },
-	  {
-		'id': 0x42f2,  // EBMLMaxIDLength
-		'data': 4
-	  },
-	  {
-		'id': 0x42f3,  // EBMLMaxSizeLength
-		'data': 8
-	  },
-	  {
-		'id': 0x4282,  // DocType
-		'data': 'webm'
-	  },
-	  {
-		'id': 0x4287,  // DocTypeVersion
-		'data': 2
-	  },
-	  {
-		'id': 0x4285,  // DocTypeReadVersion
-		'data': 2
-	  }
-	]
-  });
+interface WebMWriterOptions {
+	video?: {
+		codec: string,
+		width: number,
+		height: number
+	},
+	audio?: {
+		codec: string,
+		numberOfChannels: number,
+		sampleRate: number
+	}
+}
 
-let buffer = target.finalize();
+class WebMWriter {
+	target: WriteTarget;
+	options: WebMWriterOptions;
+	segment: EBMLElement;
+	segmentInfo: EBMLElement;
+	tracksElement: EBMLElement;
+	currentCluster: EBMLElement;
+	currentClusterTimestamp: number;
+	segmentDuration: EBMLElement;
+	audioCodecPrivate: EBMLElement;
+	cues: EBMLElement;
+	seekHead: {
+		id: number;
+		data: {
+			id: number;
+			data: ({
+				id: number;
+				data: Uint8Array;
+				size?: undefined;
+			} | {
+				id: number;
+				size: number;
+				data: number;
+			})[];
+		}[];
+	};
 
-console.log(buffer);
-saveFile(new Blob([buffer]));
+	duration = 0;
+	videoChunkQueue: EncodedVideoChunk[] = [];
+	audioChunkQueue: EncodedAudioChunk[] = [];
+	lastVideoTimestamp = 0;
+	lastAudioTimestamp = 0;
+
+	constructor(options: WebMWriterOptions) {
+		this.target = new ArrayBufferWriteTarget();
+		this.options = options;
+
+		this.writeHeader();
+	}
+
+	writeHeader() {
+		let ebmlHeader: EBML = { id: 0x1a45dfa3, data: [
+			{ id: 0x4286, data: 1 },
+			{ id: 0x42f7, data: 1 },
+			{ id: 0x42f2, data: 4 },
+			{ id: 0x42f3, data: 8 },
+			{ id: 0x4282, data: 'webm' },
+			{ id: 0x4287, data: 2 },
+			{ id: 0x4285, data: 2 }
+		] };
+		this.target.writeEBML(ebmlHeader);
+
+		let seekHead = { id: 0x114D9B74, data: [
+			{ id: 0x4DBB, data: [
+				{ id: 0x53AB, data: new Uint8Array([ 0x1C, 0x53, 0xBB, 0x6B ]) },
+				{ id: 0x53AC, size: 5, data: 0 }
+			] },
+			{ id: 0x4DBB, data: [
+				{ id: 0x53AB, data: new Uint8Array([ 0x15, 0x49, 0xA9, 0x66 ]) },
+				{ id: 0x53AC, size: 5, data: 0 }
+			] },
+			{ id: 0x4DBB, data: [
+				{ id: 0x53AB, data: new Uint8Array([ 0x16, 0x54, 0xAE, 0x6B ]) },
+				{ id: 0x53AC, size: 5, data: 0 }
+			] }
+		] };
+		this.seekHead = seekHead;
+
+		let segmentDuration: EBML = { id: 0x4489, data: new EBMLFloat64(0) };
+		this.segmentDuration = segmentDuration;
+
+		let segmentInfo: EBML = { id: 0x1549a966, data: [
+			{ id: 0x2ad7b1, data: 1e6 },
+			{ id: 0x4d80, data: "Vani's epic muxer" },
+			{ id: 0x5741, data: "Vani's epic muxer" },
+			segmentDuration
+		] };
+		this.segmentInfo = segmentInfo;
+
+		let tracksElement = { id: 0x1654ae6b, data: [] as EBML[] };
+		this.tracksElement = tracksElement;
+
+		if (this.options.video) {
+			tracksElement.data.push({ id: 0xae, data: [
+				{ id: 0xd7, data: VIDEO_TRACK_NUMBER },
+				{ id: 0x73c5, data: VIDEO_TRACK_NUMBER },
+				{ id: 0x83, data: 1 },
+				{ id: 0x86, data: this.options.video.codec },
+				{ id: 0xe0, data: [
+					{ id: 0xb0, data: this.options.video.width },
+					{ id: 0xba, data: this.options.video.height }
+				] }
+			] });
+		}
+		if (this.options.audio) {
+			this.audioCodecPrivate = { id: 0x63a2, data: new Uint8Array(19) };
+
+			tracksElement.data.push({ id: 0xae, data: [
+				{ id: 0xd7, data: AUDIO_TRACK_NUMBER },
+				{ id: 0x73c5, data: AUDIO_TRACK_NUMBER },
+				{ id: 0x83, data: 2 },
+				{ id: 0x86, data: this.options.audio.codec },
+				this.audioCodecPrivate,
+				{ id: 0xe1, data: [
+					{ id: 0xb5, data: new EBMLFloat32(this.options.audio.sampleRate) },
+					{ id: 0x9f, data: this.options.audio.numberOfChannels}
+				] }
+			] });
+		}
+
+		let segment: EBML = { id: 0x18538067, size: 5, data: [
+			seekHead,
+			segmentInfo,
+			tracksElement
+		] };
+		this.segment = segment;
+
+		this.target.writeEBML(segment);
+
+		this.cues = { id: 0x1C53BB6B, data: [] };
+	}
+
+	addVideoChunk(chunk: EncodedVideoChunk) {
+		this.lastVideoTimestamp = chunk.timestamp;
+		
+		console.log(chunk);
+
+		while (this.audioChunkQueue.length > 0 && this.audioChunkQueue[0].timestamp <= chunk.timestamp) {
+			let audioChunk = this.audioChunkQueue.shift();
+			this.writeSimpleBlock(audioChunk);
+		}
+
+		if (!this.options.audio || chunk.timestamp <= this.lastAudioTimestamp) {
+			this.writeSimpleBlock(chunk);
+			this.lastVideoTimestamp = chunk.timestamp;
+		} else {
+			this.videoChunkQueue.push(chunk);
+		}
+	}
+	
+	addAudioChunk(chunk: EncodedAudioChunk, meta: EncodedAudioChunkMetadata) {
+		this.lastAudioTimestamp = chunk.timestamp;
+		
+		console.log(chunk);
+
+		while (this.videoChunkQueue.length > 0 && this.videoChunkQueue[0].timestamp <= chunk.timestamp) {
+			let videoChunk = this.videoChunkQueue.shift();
+			this.writeSimpleBlock(videoChunk);
+		}
+
+		if (!this.options.video || chunk.timestamp <= this.lastVideoTimestamp) {
+			this.writeSimpleBlock(chunk);
+			this.lastAudioTimestamp = chunk.timestamp;
+		} else {
+			this.audioChunkQueue.push(chunk);
+		}
+
+		if (meta?.decoderConfig) {
+			this.audioCodecPrivate.data = new Uint8Array(meta.decoderConfig.description as any);
+			let endPos = this.target.pos;
+			this.target.seek(this.target.offsets.get(this.audioCodecPrivate));
+			this.target.writeEBML(this.audioCodecPrivate);
+			this.target.seek(endPos);
+		}
+	}
+
+	writeSimpleBlock(chunk: EncodedVideoChunk | EncodedAudioChunk) {
+		let msTime = Math.floor(chunk.timestamp / 1000);
+
+		if (!this.currentCluster || (chunk instanceof EncodedVideoChunk && chunk.type === 'key' || msTime - this.currentClusterTimestamp >= MAX_CHUNK_LENGTH_MS)) {
+			this.createNewCluster(msTime);
+		}
+
+		let prelude = new Uint8Array(4);
+		let view = new DataView(prelude.buffer);
+
+		view.setUint8(0, 0x80 | ((chunk instanceof EncodedVideoChunk) ? VIDEO_TRACK_NUMBER : AUDIO_TRACK_NUMBER));
+		view.setUint16(1, msTime - this.currentClusterTimestamp, false);
+		view.setUint8(3, Number(chunk.type === 'key') << 7); // Flags
+
+		let data = new Uint8Array(chunk.byteLength);
+		chunk.copyTo(data);
+
+		let simpleBlock = { id: 0xA3, data: [
+			prelude,
+			data
+		] };
+		this.target.writeEBML(simpleBlock);
+
+		this.duration = Math.max(this.duration, msTime);
+	}
+
+	createNewCluster(timestamp: number) {
+		if (this.currentCluster) {
+			this.finalizeCurrentCluster();
+		}
+
+		this.currentCluster = { id: 0x1f43b675, data: [
+			{ id: 0xe7, data: timestamp }
+		] };
+		this.target.writeEBML(this.currentCluster);
+
+		this.currentClusterTimestamp = timestamp;
+
+		(this.cues.data as EBML[]).push({ id: 0xBB, data: [
+			{ id: 0xB3, data: timestamp },
+			{ id: 0xB7, data: [
+				{ id: 0xF7, data: VIDEO_TRACK_NUMBER },
+				{ id: 0xF1, data: this.target.offsets.get(this.currentCluster) - (this.target.offsets.get(this.segment) + 8) }
+			] }
+		] });
+	}
+
+	finalizeCurrentCluster() {
+		let clusterSize = this.target.pos - (this.target.offsets.get(this.currentCluster) + 8);
+		let endPos = this.target.pos;
+
+		this.target.seek(this.target.offsets.get(this.currentCluster) + 4);
+		this.target.writeEBMLVarInt(clusterSize, 4);
+		this.target.seek(endPos);
+	}
+
+	finalize() {
+		while (this.videoChunkQueue.length > 0) this.writeSimpleBlock(this.videoChunkQueue.shift());
+		while (this.audioChunkQueue.length > 0) this.writeSimpleBlock(this.audioChunkQueue.shift());
+
+		this.finalizeCurrentCluster();
+		this.target.writeEBML(this.cues);
+
+		let endPos = this.target.pos;
+
+		let segmentSize = this.target.pos - (this.target.offsets.get(this.segment) + 8);
+		this.target.seek(this.target.offsets.get(this.segment) + 4);
+		this.target.writeEBMLVarInt(segmentSize, 4);
+
+		this.segmentDuration.data = new EBMLFloat64(this.duration);
+		this.target.seek(this.target.offsets.get(this.segmentDuration));
+		this.target.writeEBML(this.segmentDuration);
+
+		this.seekHead.data[0].data[1].data = this.target.offsets.get(this.cues) - (this.target.offsets.get(this.segment) + 8);
+		this.seekHead.data[1].data[1].data = this.target.offsets.get(this.segmentInfo) - (this.target.offsets.get(this.segment) + 8);
+		this.seekHead.data[2].data[1].data = this.target.offsets.get(this.tracksElement) - (this.target.offsets.get(this.segment) + 8);
+
+		this.target.seek(this.target.offsets.get(this.seekHead));
+		this.target.writeEBML(this.seekHead);
+
+		this.target.seek(endPos);
+	}
+}
+
+(async () => {
+	let sampleRate = 48000;
+
+	let writer = new WebMWriter({
+		video: {
+			codec: 'V_VP9',
+			width: 1280,
+			height: 720
+		},
+		audio: {
+			codec: 'A_OPUS',
+			numberOfChannels: 1,
+			sampleRate
+		}
+	});
+	
+	let canvas = document.createElement('canvas');
+	canvas.setAttribute('width', '1280');
+	canvas.setAttribute('height', '720');
+	let ctx = canvas.getContext('2d');
+	
+	let videoEncoder = new VideoEncoder({
+		output: chunk => writer.addVideoChunk(chunk),
+		error: e => console.error(e)
+	});
+	videoEncoder.configure({
+		codec: 'vp09.00.10.08',
+		width: 1280, 
+		height: 720,
+		bitrate: 1e6
+	});
+
+	let audioEncoder = new AudioEncoder({
+		output: (chunk, meta) => writer.addAudioChunk(chunk, meta),
+		error: e => console.error(e)
+	});
+	audioEncoder.configure({
+		codec: 'opus',
+		numberOfChannels: 1,
+		sampleRate,
+		bitrate: 32000,
+	});
+
+	let audioContext = new AudioContext();
+	let audioBuffer = await audioContext.decodeAudioData(await (await fetch('./CantinaBand60.wav')).arrayBuffer());
+	let length = 5;
+	let data = new Float32Array(length * sampleRate);
+	data.set(audioBuffer.getChannelData(0).subarray(0, data.length), 0);
+
+	let audioData = new AudioData({
+		format: 'f32',
+		sampleRate,
+		numberOfFrames: length * sampleRate,
+		numberOfChannels: 1,
+		timestamp: 0,
+		data: data
+	});
+	audioEncoder.encode(audioData);
+	audioData.close();
+	
+	for (let i = 0; i < length * 5; i++) {
+		ctx.fillStyle = ['red', 'lime', 'blue', 'yellow'][Math.floor(Math.random() * 4)];
+		ctx.fillRect(Math.random() * 1280, Math.random() * 720, Math.random() * 1280, Math.random() * 720);
+
+		let videoFrame = new VideoFrame(canvas, { timestamp: i * 1000000/5 });
+		videoEncoder.encode(videoFrame);
+		videoFrame.close();
+	}
+
+	await Promise.allSettled([videoEncoder.flush(), audioEncoder.flush()]);
+
+	writer.finalize();
+	
+	let buffer = (writer.target as ArrayBufferWriteTarget).finalize();
+	
+	console.log(buffer);
+	saveFile(new Blob([buffer]));
+})();
