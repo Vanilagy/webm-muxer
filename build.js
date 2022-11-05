74ebfa394f0e7898242f2d575ac19402d0094e5b
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2022-11-05 22:20:42 +0100

    Change build file names

--- build.js
+++ build.js
@@ -3,7 +3,16 @@ const esbuild = require('esbuild');
 esbuild.build({
 	entryPoints: ['src/main.ts'],
 	bundle: true,
-	outfile: 'build/main.js',
+	outfile: 'build/webm-muxer.js',
 	logLevel: 'info',
 	watch: true
+});
+
+esbuild.build({
+	entryPoints: ['src/main.ts'],
+	bundle: true,
+	outfile: 'build/webm-muxer.min.js',
+	logLevel: 'info',
+	watch: true,
+	minify: true
 });
