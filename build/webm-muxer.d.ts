d86d882334c46a4d2eed43654b860edbc12a6ca6
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2022-11-10 20:39:35 +0100

    Fix declaration file, once again

--- build/webm-muxer.d.ts
+++ build/webm-muxer.d.ts
@@ -14,18 +14,14 @@ declare interface WebMMuxerOptions {
 	}
 }
 
-declare class WebMMuxer {
-	constructor(options: WebMMuxerOptions);
-
-	addVideoChunk(chunk: EncodedVideoChunk, meta: EncodedVideoChunkMetadata, timestamp?: number): void;	
-	addAudioChunk(chunk: EncodedAudioChunk, meta: EncodedAudioChunkMetadata, timestamp?: number): void;
-	finalize(): ArrayBuffer | null;
-}
-
-type WebMMuxerClass = typeof WebMMuxer;
-
 declare global {
-	var WebMMuxer: WebMMuxerClass;
+	class WebMMuxer {
+		constructor(options: WebMMuxerOptions);
+	
+		addVideoChunk(chunk: EncodedVideoChunk, meta: EncodedVideoChunkMetadata, timestamp?: number): void;	
+		addAudioChunk(chunk: EncodedAudioChunk, meta: EncodedAudioChunkMetadata, timestamp?: number): void;
+		finalize(): ArrayBuffer | null;
+	}
 }
 
 export = WebMMuxer;
